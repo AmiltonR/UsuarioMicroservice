7d29a79fca293e0886cbb2912d24cdf95d465512
AmiltonR <32588294+AmiltonR@users.noreply.github.com>
2022-10-20 03:46:37 -0600

    Adding Instructor, Grados and Habilidades Entities

---
+++ UserDbContext.Domain/Models/Entities/Instructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDbContext.Domain.Models.Entities
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public int IdGradoAcademico { get; set; }
+        public string Perfil { get; set; }
+    }
+}
